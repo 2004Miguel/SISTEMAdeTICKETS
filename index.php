ac599c30fad7349b6d67069395c1fe3725a8fbbf
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2024-01-15 12:07:10 -0500

    se creó el index php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de sesio</title>
+</head>
+<body>
+    <h1>INICIO DE SESION</h1>
+    
+</body>
+</html>
