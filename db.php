4b5e8ab9c1ce6da5810ffbc50bae8a575e56f822
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2024-01-17 11:51:13 -0500

    verificar al momento de hacer un insert. Que no aparezcan estos 2 mensajes al mismo tiempo: Error al conectar con la db y Usuario ingresado con exito

--- db.php
+++ db.php
@@ -14,13 +14,25 @@ class database{
     }
 
     public function conectar_db(){
-        $con=mysqli_connect($this->server, $this->user, $this->password, $this->db);
+        $this->con=mysqli_connect($this->server, $this->user, $this->password, $this->db);
 
         if($con){
             echo "CONEXIÓN EXITOSA";
         }else{
             echo "Error al conectar con la base de datos";
         }
+
+    }
+
+    public function registrar($apellidop, $cargop, $contrasenap, $emailp, $nombrep, $idp){
+        $query="INSERT INTO usuario (apellido, cargo, contrasena, email, id_usuario, nombre) VALUES ('$apellidop', '$cargop', '$contrasenap', '$emailp', '$idp', '$nombrep')";
+        $resul=mysqli_query($this->con, $query);
+        if($resul){
+            print("Usuario registrado con exito");
+        }else{
+            print("Error al registrar el usuario");
+        }
+
 
     }
 
--- registro.php
+++ registro.php
@@ -9,6 +9,14 @@ if(isset($_POST["return"])){
 }
 
 if(isset($_POST["btn_registrar"])){
+    $nombre=$_POST["txt_userRegister"];
+    $apellido=$_POST["surname"];
+    $cargo=$_POST["cargo"];
+    $email=$_POST["email"];
+    $contrasena=$_POST["txt_password"];
+    $id=$_POST["iden"];
+
+    $ob2->registrar($apellido, $cargo, $contrasena, $email, $nombre, $id);
     
 }
 
@@ -28,27 +36,31 @@ if(isset($_POST["btn_registrar"])){
     
     <form action="" method="post">
         <label for="txt_userRegister">Ingrese su <b>nombre</b> o alías por favor</label>
-        <input type="text" id="txt_userRegister" name="txt_userRegister" required>
+        <input type="text" id="txt_userRegister" name="txt_userRegister" autocomplete="name" >
         <br></br>
 
         <label for="surname">Ingrese sus <b>apellidos</b> por favor</label>
-        <input type="text" name="surname" id="surname" required>
+        <input type="text" name="surname" id="surname" autocomplete="family-name" >
         <br></br>
 
         <label for="cargo">Insgrese su <b>cargo</b> por favor</label>
-        <input type="text" name="cargo" id="cargo" required>
+        <input type="text" name="cargo" id="cargo" >
         <br></br>
 
         <label for="email">Ingrese su <b>email</b> por favor</label>
-        <input type="email" name="email" id="email" required>
+        <input type="email" name="email" id="email" autocomplete="email" >
+        <br></br>
+
+        <label for="iden">Ingrese su identificación por favor</label>
+        <input type="number" name="iden" id="iden">
         <br></br>
 
         <label for="">Ingrese una <b>constraseña</b> segura</label>
-        <input type="text" id="txt_password" name="txt_password" required>
+        <input type="text" id="txt_password" name="txt_password" autocomplete="current-password" >
         <br></br>
 
         <input type="submit" name="btn_registrar" value="REGISTRAR">
-        <input type="submit" value="VOLVER" name="return" required>
+        <input type="submit" value="VOLVER" name="return" >
     </form>
 
     
